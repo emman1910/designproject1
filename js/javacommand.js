f1ef44b174d8d4482aa71cd5afdf7b9a8bfbd2e6
emman1910 <emman1910@gmail.com>
2017-11-23 12:48:40 +0800

    designproject1
    css and html first project to publish with responsive style, carousel and lightbox feature.

---
+++ js/javacommand.js
@@ -0,0 +1,44 @@
+// MENU for Mobile 
+var nav = false;
+
+function show_hide_nav(id) {
+    var navbar = document.getElementById(id);
+    if (nav == false) {
+        navbar.style.display = "block";
+        nav = true;
+    } else {
+        navbar.style.display = "none";
+        nav = false;
+    }
+}
+// QOUTE SECTION 
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
